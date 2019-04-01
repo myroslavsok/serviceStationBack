3a39d703641279241893b9feb33a5945cf286a60
InterLink Intern <intern@intern-pc.interlink.office>
2019-04-01 10:29:46 +0300

    Deploy 2

--- src/main/java/com/example/demo/controllers/ModelController.java
+++ src/main/java/com/example/demo/controllers/ModelController.java
@@ -19,7 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("models")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 public class ModelController {
 
     @Autowired
--- src/main/java/com/example/demo/controllers/OrderController.java
+++ src/main/java/com/example/demo/controllers/OrderController.java
@@ -19,7 +19,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 @RestController
 @RequestMapping("orders")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 public class OrderController {
 
     @Autowired
